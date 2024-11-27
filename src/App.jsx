b9b015c2194d00606dd5ec5dc7a8408f9c9d8bdd
agus setyawan <agsn.does6@gmail.com>
2024-11-28 00:42:40 +0700

    chore: setting up tailwind in vite

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,9 @@
 function App() {
-  return <main>Hello World!</main>;
+  return (
+    <main>
+      <h1 className="text-indigo-500 font-semibold text-5xl">Hello World!</h1>
+    </main>
+  );
 }
 
 export default App;
