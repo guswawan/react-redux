2f1220d3e6be8afa94e4077bf04a0c0c20f1f40b
agus setyawan <agsn.does6@gmail.com>
2024-11-29 01:21:49 +0700

    fix(style): package dependencies and header component

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,7 +1,30 @@
+import { Button } from './ui/button';
+import { Input } from './ui/input';
+import { IoCart, IoHeart } from 'react-icons/io5';
+import { Separator } from './ui/separator';
+
 export const Header = () => {
   return (
-    <header className="w-full font-semibold text-xl flex justify-center py-4 border-b-2">
-      My Custom Header
+    <header className="flex justify-between items-center h-16 px-24 border-b-2">
+      <p className="font-extrabold text-2xl hover:cursor-pointer">
+        OnWinkelsite
+      </p>
+      <Input className="max-w-[50%]" placeholder="Search Product..." />
+      <div className="flex space-x-4 h-6 items-center">
+        <div>
+          <Button size="icon" variant="ghost">
+            <IoCart size={30} />
+          </Button>
+          <Button size="icon" variant="ghost">
+            <IoHeart className="w-6 h-6" />
+          </Button>
+        </div>
+        <Separator orientation="vertical h-full" />
+        <div className="space-x-2">
+          <Button>Log In</Button>
+          <Button variant="outline">Sign Up</Button>
+        </div>
+      </div>
     </header>
   );
 };
